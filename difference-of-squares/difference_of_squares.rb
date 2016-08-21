505fe050fc0e35be4a68d3c3556edfb055de129b
Artur <artur.prado@gmail.com>
2016-08-21 13:34:44 -0300

    Solve difference of squares

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,21 @@
+class Squares
+  def initialize(number)
+    @number = number
+  end
+
+  def difference
+    @difference ||= square_of_sum - sum_of_squares
+  end
+
+  def square_of_sum
+    @number.downto(0).reduce(&:+) ** 2
+  end
+
+  def sum_of_squares
+    @number.downto(1).reduce(0) { |sum, n| sum += n ** 2 }
+  end
+end
+
+module BookKeeping
+  VERSION = 3.freeze
+end
