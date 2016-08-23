595928958965c899ba444c401cbaaf261be76e41
Artur <artur.prado@gmail.com>
2016-08-22 21:10:33 -0300

    Improve rna transcript solution

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -3,19 +3,10 @@ module BookKeeping
 end
 
 class Complement
-  MAP = {
-    'G' => 'C',
-    'C' => 'G',
-    'T' => 'A',
-    'A' => 'U'
-  }
-
   def self.of_dna(strand)
     return '' if invalid_strand?(strand)
 
-    strand.to_a.map do |nucleotide|
-      MAP[nucleotide]
-    end.join('')
+    strand.tr('GCTA', 'CGAU')
   end
 
   def self.invalid_strand?(strand)
